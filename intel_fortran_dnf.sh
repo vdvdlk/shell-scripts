33653cf50ca500e743179f6bda4783ee4092fa22
Thales Freitas Macedo <thalesfreitas96@gmail.com>
2024-02-03 17:38:29 -0300

    Removed the "Add variables" step

--- intel_fortran_dnf.sh
+++ intel_fortran_dnf.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # This script installs the Intel Fortran compilers, ifx and ifort,
-# and the llapack and blas libraries, mkl with the dnf package manager.
+# and the lapack and blas libraries, mkl with the dnf package manager.
 
 # Instructions taken from
 # https://www.intel.com/content/www/us/en/docs/oneapi/installation-guide-linux/2024-0/yum-dnf-zypper.html
 # https://www.intel.com/content/www/us/en/docs/fortran-compiler/get-started-guide/2022-2/get-started-on-linux.html
 
 # This script should be run as sudo:
-# sudo ./intel_fortran_dnf.sh
+# sudo bash intel_fortran_dnf.sh
 # This script assumes that you don't have an existing installation
 
 # Create a temporary repo file
-tee > /tmp/oneAPI.repo << EOF
+tee >/tmp/oneAPI.repo <<EOF
 [oneAPI]
 name=Intel® oneAPI repository
 baseurl=https://yum.repos.intel.com/oneapi
@@ -27,17 +27,3 @@ mv /tmp/oneAPI.repo /etc/yum.repos.d
 
 # Install ifx, ifort and mkl
 dnf install intel-oneapi-compiler-fortran intel-oneapi-mkl -y
-
-# Set the environment variables
-# ATTENTION: THIS STEP IS DONE FOR THE CURRENT USER ONLY
-if grep -Fxq "source /opt/intel/oneapi/setvars.sh" /home/"$SUDO_USER"/.bash_profile
-then
-    echo "Environment variables already set"
-    echo ""
-else
-    echo "Setting environment variables"
-    { echo ""; echo "source /opt/intel/oneapi/setvars.sh"; } >> /home/"$SUDO_USER"/.bash_profile
-    echo "Environment variables ready"
-    echo ""
-fi
-# Restart the system or log off and log in again to take effect
