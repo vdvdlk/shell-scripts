47c8eaa5fe42d844774dcc0fd1e192ea479f05a8
Thales Freitas Macedo <thalesfreitas96@gmail.com>
2024-04-28 12:28:38 -0300

    Add Fortran install script

--- debian-12/fortran-install.sh
+++ debian-12/fortran-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # THIS SCRIPT SHOULD BE RUN AS SUDO!
 
-apt-get install --assume-yes gfortran fortls fortran-language-server findent
+apt-get install --assume-yes gfortran fortran-language-server findent
