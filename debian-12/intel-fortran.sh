c66615b05f5fd853e998bc7c5062f1fe71e46ea1
Thales Freitas Macedo <39356150+vdvdlk@users.noreply.github.com>
2024-10-23 18:31:20 -0300

    Update intel-fortran.sh
    Updated the key download command to match the new instructions.

--- debian-12/intel-fortran.sh
+++ debian-12/intel-fortran.sh
@@ -7,12 +7,14 @@
 # Instructions taken from
 # https://www.intel.com/content/www/us/en/docs/oneapi/installation-guide-linux/2024-0/apt.html
 # https://www.intel.com/content/www/us/en/docs/fortran-compiler/get-started-guide/2022-2/get-started-on-linux.html
+# https://www.intel.com/content/www/us/en/developer/tools/oneapi/fortran-compiler-download.html?operatingsystem=linux&distribution-linux=apt
 
-keyserver="https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB"
-keypath="/etc/apt/keyrings/oneapi-archive-keyring.gpg"
+key="https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB"
+keyring="/etc/apt/keyrings/oneapi-archive-keyring.gpg"
 
 # Download the key to system keyring
-curl -sS $keyserver | gpg --dearmor --yes -o $keypath
+# curl -sS $key | gpg --dearmor --yes -o $keyring
+wget -O- $key | gpg --dearmor | tee $keyring > /dev/null
 
 # Creates the repo file
 tee >/etc/apt/sources.list.d/oneAPI.sources <<EOF
@@ -22,7 +24,7 @@ Types: deb
 URIs: https://apt.repos.intel.com/oneapi
 Suites: all
 Components: main
-Signed-By: $keypath
+Signed-By: $keyring
 EOF
 
 # Limits what the repo can install
